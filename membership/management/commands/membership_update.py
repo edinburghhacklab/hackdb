2af78d1d1fb79df6dedb2b54e066db5875735972
Tim Hawes <me@timhawes.com>
2022-04-25 11:36:20 +0100

    membership: daily update script

---
+++ membership/management/commands/membership_update.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2022 Tim Hawes <me@timhawes.com>
+#
+# SPDX-License-Identifier: MIT
+
+from django.core.management.base import BaseCommand
+
+from membership.models import Member
+
+
+class Command(BaseCommand):
+    help = "Update the membership status and members group"
+
+    def handle(self, *args, **options):
+        for member in Member:
+            if member.fixup():
+                member.save()
