6ebb5d39920c3973bae5a03bdda202730c2d16cb
Tim Hawes <me@timhawes.com>
2023-02-06 16:37:45 +0000

    Customise username validation

---
+++ hackdb/validators.py
@@ -0,0 +1,17 @@
+import re
+
+from django.core import validators
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import gettext_lazy as _
+
+
+@deconstructible
+class HacklabUsernameValidator(validators.RegexValidator):
+    regex = r"^[A-Za-z][A-Za-z0-9_.-]*\Z"
+    message = _(
+        "Enter a valid username. This value may contain only unaccented lowercase a-z "
+        "and uppercase A-Z letters, numbers, and ./-/_ characters. It must begin with a letter."
+    )
+    flags = re.ASCII
+
+custom_username_validators = [HacklabUsernameValidator()]
