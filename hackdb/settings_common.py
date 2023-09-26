37746acd2814807731a540976fffcfbfb598cb30
Tim Hawes <me@timhawes.com>
2023-09-26 21:59:56 +0100

    Update dependencies

--- hackdb/settings_common.py
+++ hackdb/settings_common.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     "apikeys.middleware.APIKeyMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "hackdb.urls"
