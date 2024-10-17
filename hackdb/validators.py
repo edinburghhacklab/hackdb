a813e18bc35907d6642545b25a7e8c8169e0a344
Tim Hawes <me@timhawes.com>
2024-10-17 20:30:18 +0100

    Enforce username size of 2-32 characters

--- hackdb/validators.py
+++ hackdb/validators.py
@@ -7,9 +7,9 @@ from django.utils.translation import gettext_lazy as _
 
 @deconstructible
 class HacklabUsernameValidator(validators.RegexValidator):
-    regex = r"^[A-Za-z][A-Za-z0-9_.-]*\Z"
+    regex = r"^[A-Za-z][A-Za-z0-9_.-]{1,31}\Z"
     message = _(
-        "Enter a valid username. This value may contain only unaccented lowercase a-z "
+        "Enter a valid username of 2 to 32 characters. This value may contain only unaccented lowercase a-z "
         "and uppercase A-Z letters, numbers, and ./-/_ characters. It must begin with a letter."
     )
     flags = re.ASCII
